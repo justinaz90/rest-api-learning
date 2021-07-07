138a0e072fe9c86260b4972aaaebcc8bc4a6bb92
Justyna Szewczyk <justyna0829@gmail.com>
2021-07-07 18:41:31 +0200

    Prepare files for Heroku publication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,15 +13,20 @@ const seatRoutes = require('./routes/seats.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonalsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
