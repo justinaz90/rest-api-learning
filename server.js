00ae0f2e0ab2f695ac98e8900ddd1101cceb3b9a
Justyna Szewczyk <justyna0829@gmail.com>
2021-08-19 14:43:38 +0200

    Add security packages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const testimonalsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatRoutes = require('./routes/seats.routes');
 
+app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,7 +34,12 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-// connects our backend code with the database
+// const dbURI = process.env.NODE_ENV === 'production' 
+//   ? 'mongodb+srv://justyna-szewczyk:testapp@cluster0.30cel.mongodb.net/NewWaveDB?retryWrites=true&w=majority'
+//   : 'mongodb://localhost:27017/NewWaveFestivalDB';
+
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 mongoose.connect('mongodb+srv://justyna-szewczyk:testapp@cluster0.30cel.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
@@ -45,6 +52,8 @@ db.on('error', err => console.log('Error ' + err));
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
+
+// module.exports = server;
 
 const io = socket(server, { 
   cors: { 
